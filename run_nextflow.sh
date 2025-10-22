0f989970156b0ad0bc1b2efd47550fe8617d35b9
apdavid <abeldavidmd@gmail.com>
2025-10-21 23:41:45 -0500

    update run_nextflow wrapper

---
+++ run_nextflow.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=100G
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-12:00:00
+#SBATCH --output=nextflow-spliz.slurm.%a.stdout
+#SBATCH --array=1
+#SBATCH --mail-user=abel.david@meei.harvard.edu
+#SBATCH --mail-type=ALL
+#SBATCH --job-name="NF-SPLIZ"
+#SBATCH --account="l2_jan_lab"
+
+echo "Log -- Start Nextflow Spliz Job"
+echo "Log -- ================="  
+echo "Log -- Date: $(date)"
+
+conda activate nf-spliz-env
+# nextflow run salzmanlab/spliz -r main -latest -c petipre_sgn_2022.config -N abel.david@vanderbilt.edu
+nextflow run . -c petipre_sgn.config -N abel.david@vanderbilt.edu
+
+echo "Log -- Date: $(date)"
+echo "Log -- End Job" 
+
+
+
+
+
